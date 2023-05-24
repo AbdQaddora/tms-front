a8eaf2c9a42586e40bedb6d293afec6f5fa7c6db
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-05-24 12:39:07 +0300

    add append notifications function

--- advisor/js/scripts.js
+++ advisor/js/scripts.js
@@ -43,16 +43,44 @@ window.addEventListener('DOMContentLoaded', event => {
     // toggle notifications
     const notifications_bell = document.getElementById("notifications_bell");
     const notifications = document.getElementById("notifications");
+    const notifications_count = document.getElementById("notifications_count");
+    // mock data at first
+    let notifications_list = ["Hello World", "Hello World 2"];
+
     let closeTimeOutId = null;
 
-    if (notifications_bell && notifications) {
+
+    if (notifications_bell && notifications && notifications_count) {
+
+        const updateNotificationsCount = (count) => {
+            notifications_count.innerText = count > 99 ? "+99" : count;
+        }
+
+        const appendNotification = (notification_text) => {
+            updateNotificationsCount(notifications_list.length);
+            updateNotificationsCount(notifications_list.length);
+
+            const notificationItem = document.createElement("div");
+            notificationItem.classList = "notification_item p-3 pt-2 pb-0";
+
+            const notificationText = document.createElement("p")
+            notificationText.classList = "notification_text"
+            notificationText.innerText = notification_text;
+
+            notificationItem.appendChild(notificationText);
+            notifications.appendChild(notificationItem);
+        }
+
+        // show initial data
+        notifications_list.map(el => appendNotification(el));
+
         notifications_bell.addEventListener('click', (e) => {
             notifications.classList.toggle('active');
             if (closeTimeOutId) {
                 clearTimeout(closeTimeOutId);
                 closeTimeOutId = null;
             }
-            
+
             closeTimeOutId = setTimeout(() => {
                 notifications.classList.remove('active');
                 closeTimeOutId = null;
